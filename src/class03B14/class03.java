d975464ea36c0d6e78ff141f8da32d950b811b7c
Eisrael03 <eisrael2203@gmail.com>
2022-09-27 19:52:19 -0400

    second commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -4,7 +4,7 @@ public class class03 {
 
 	public static void main(String[] args) {
 	System.out.println("Hey this is a git class");
-
+	System.out.println("This is a change in my project");
 	}
 
 }
